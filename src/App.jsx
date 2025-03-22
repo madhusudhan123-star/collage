e195f11c54578c87981e49cf86301b015220fc22
madhusudhan123-star <dmadhusudhan98@gmail.com>
2025-03-22 12:48:04 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,66 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import { Toaster } from 'react-hot-toast';
+// import Navbar from './components/Navbar';
+// import Home from './pages/Home';
+// import About from './pages/About';
+// import CA_final from './pages/courses/ca/cafinal/CA_final';
+// import Footer from './components/Footer';
+// import Faq from './pages/Faq';
+// import  Request  from './pages/Request';
+// import CA_inter from './pages/courses/ca/cainter/CA_inter';
+// import Contact from './pages/Contact';
+// import Enroll from './pages/Enroll';
+// import Meccec from './pages/courses/Meccec';
+// import Degree from './pages/courses/Degree';
+// import ACCA from './pages/courses/ACCA';
+// import CA_found from './pages/courses/ca/cafound/CA_found';
+// import CMA_final from './pages/courses/cma/cmafinal/CMA_final';
+// import CMA_inter from './pages/courses/cma/cmainter/CMA_inter';
+// import CMA_found from './pages/courses/cma/cmafound/CMA_found';
+// import CA_test from './pages/test_series/CA_test';
+// import Cma from './pages/courses/Cma';
+// import Ca from './pages/courses/Ca'
+
+
+// function App() {
+//   return (
+//     <Router>
+//       <div className="flex flex-col min-h-screen">
+//         <Toaster position="top-center" reverseOrder={false} />
+//         <Navbar />
+//         <main className="flex-grow">
+//           <Routes>
+//             <Route path="/" element={<Home />} />
+//             <Route path="/about" element={<About />} />
+//             <Route path="/faq" element={<Faq />} />
+//             <Route path="/courses/cafinal" element={<CA_final />} />
+//             <Route path="/courses/cainter" element={<CA_inter />} />
+//             <Route path="/courses/cafoundation" element={<CA_found />} />
+//             <Route path="/courses/cmafinal" element={<CMA_final />} />
+//             <Route path="/courses/cmainter" element={<CMA_inter />} />
+//             <Route path="/courses/cmafoundation" element={<CMA_found />} />
+//             <Route path="/request" element={<Request />} />
+//             <Route path="/contact" element={<Contact />} />
+//             <Route path="/enroll" element={<Enroll />} />
+//             <Route path="/courses/mec" element={<Meccec />} />
+//             <Route path="/courses/degree" element={<Degree />} />
+//             <Route path="/courses/acca" element={<ACCA />} />
+//             <Route path="/test/cafinal" element={<CA_test />} />
+//             <Route path='/cma' element={<Cma />} />
+//             <Route path='/ca' element={<Ca />} />
+            
+//           </Routes>
+//         </main>
+//         <Footer />
+//       </div>
+//     </Router>
+//   );
+// }
+
+// export default App;
+
+
+import { Routes, Route } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
@@ -6,7 +68,7 @@ import About from './pages/About';
 import CA_final from './pages/courses/ca/cafinal/CA_final';
 import Footer from './components/Footer';
 import Faq from './pages/Faq';
-import  Request  from './pages/Request';
+import Request from './pages/Request';
 import CA_inter from './pages/courses/ca/cainter/CA_inter';
 import Contact from './pages/Contact';
 import Enroll from './pages/Enroll';
@@ -19,41 +81,37 @@ import CMA_inter from './pages/courses/cma/cmainter/CMA_inter';
 import CMA_found from './pages/courses/cma/cmafound/CMA_found';
 import CA_test from './pages/test_series/CA_test';
 import Cma from './pages/courses/Cma';
-import Ca from './pages/courses/Ca'
-
+import Ca from './pages/courses/Ca';
 
 function App() {
   return (
-    <Router>
-      <div className="flex flex-col min-h-screen">
-        <Toaster position="top-center" reverseOrder={false} />
-        <Navbar />
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/faq" element={<Faq />} />
-            <Route path="/courses/cafinal" element={<CA_final />} />
-            <Route path="/courses/cainter" element={<CA_inter />} />
-            <Route path="/courses/cafoundation" element={<CA_found />} />
-            <Route path="/courses/cmafinal" element={<CMA_final />} />
-            <Route path="/courses/cmainter" element={<CMA_inter />} />
-            <Route path="/courses/cmafoundation" element={<CMA_found />} />
-            <Route path="/request" element={<Request />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/enroll" element={<Enroll />} />
-            <Route path="/courses/mec" element={<Meccec />} />
-            <Route path="/courses/degree" element={<Degree />} />
-            <Route path="/courses/acca" element={<ACCA />} />
-            <Route path="/test/cafinal" element={<CA_test />} />
-            <Route path='/cma' element={<Cma />} />
-            <Route path='/ca' element={<Ca />} />
-            
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <div className="flex flex-col min-h-screen">
+      <Toaster position="top-center" reverseOrder={false} />
+      <Navbar />
+      <main className="flex-grow">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/faq" element={<Faq />} />
+          <Route path="/courses/cafinal" element={<CA_final />} />
+          <Route path="/courses/cainter" element={<CA_inter />} />
+          <Route path="/courses/cafoundation" element={<CA_found />} />
+          <Route path="/courses/cmafinal" element={<CMA_final />} />
+          <Route path="/courses/cmainter" element={<CMA_inter />} />
+          <Route path="/courses/cmafoundation" element={<CMA_found />} />
+          <Route path="/request" element={<Request />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/enroll" element={<Enroll />} />
+          <Route path="/courses/mec" element={<Meccec />} />
+          <Route path="/courses/degree" element={<Degree />} />
+          <Route path="/courses/acca" element={<ACCA />} />
+          <Route path="/test/cafinal" element={<CA_test />} />
+          <Route path='/cma' element={<Cma />} />
+          <Route path='/ca' element={<Ca />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
   );
 }
 
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,20 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     port: 3000
+//   }
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000
+    port: 3000,
+    historyApiFallback: true // Fix 404 errors on refresh
   }
 })
